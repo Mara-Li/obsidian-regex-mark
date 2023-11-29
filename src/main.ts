f35c5b95a8c62e9aa86fd92723b6699d34ca910c
Mara <mara-li@outlook.fr>
2023-11-29 15:19:38 +0100

    feat: reload extension when settings change

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,45 @@
-import { Plugin } from 'obsidian'
-import { MarkdownProcesser } from './markdownProcesser'
-import { cmExtension } from './cmPlugin'
-import { RemarkRegexSettingTab, SettingOptions } from './setting'
-import { Extension } from '@codemirror/state'
+import { Extension } from "@codemirror/state";
+import { Plugin } from "obsidian";
+
+import { cmExtension } from "./cmPlugin";
+import { MarkdownProcesser } from "./markdownProcesser";
+import { RemarkRegexSettingTab, SettingOptions } from "./setting";
 
 export default class RegexMark extends Plugin {
-  settings: SettingOptions = []
-  extensions: Extension[]
-  cmExtension: Extension
+	settings: SettingOptions = [];
+	extensions: Extension[];
+	cmExtension: Extension;
 
-  async onload() {
-    console.log('loading plugin RegexMark')
-    await this.loadSettings()
-    this.addSettingTab(new RemarkRegexSettingTab(this.app, this))
-    this.registerMarkdownPostProcessor((element: HTMLElement) => {
-      MarkdownProcesser(this.settings, element)
-    })
-    this.cmExtension = cmExtension(this)
-    this.extensions = []
-    this.updateCmExtension()
-    this.registerEditorExtension(this.extensions)
-  }
+	async onload() {
+		console.log("loading plugin RegexMark");
+		await this.loadSettings();
+		this.addSettingTab(new RemarkRegexSettingTab(this.app, this));
+		this.registerMarkdownPostProcessor((element: HTMLElement) => {
+			MarkdownProcesser(this.settings, element);
+		});
+		this.cmExtension = cmExtension(this);
+		this.extensions = [];
+		this.updateCmExtension();
+		this.registerEditorExtension(this.extensions);
+	}
 
-  onunload() {
-    console.log('unloading plugin RegexMark')
-  }
+	onunload() {
+		console.log("unloading plugin RegexMark");
+	}
 
-  async loadSettings() {
-    this.settings = Object.assign([], [], await this.loadData())
-  }
+	async loadSettings() {
+		this.settings = Object.assign([], [], await this.loadData());
+	}
 
-  async saveSettings() {
-    await this.saveData(this.settings)
-  }
+	async saveSettings() {
+		await this.saveData(this.settings);
+		this.updateCmExtension();
+	}
 
-  updateCmExtension() {
-    this.extensions.remove(this.cmExtension)
-    this.cmExtension = cmExtension(this)
-    this.extensions.push(this.cmExtension)
-    this.app.workspace.updateOptions()
-  }
+	updateCmExtension() {
+		this.extensions.remove(this.cmExtension);
+		this.cmExtension = cmExtension(this);
+		this.extensions.push(this.cmExtension);
+		this.app.workspace.updateOptions();
+	}
 }
